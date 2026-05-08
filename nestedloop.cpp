dcda5b11de0411f886cc04e3a75c803df3f430e2
ughte <patiencenarh347@gmail.com>
2026-05-08 19:05:43 +0000

    nestedloops

---
+++ nestedloop.cpp
@@ -0,0 +1,39 @@
+// #include <iostream>
+// int main(){
+//     for(int i=1; i<=3 ;i++){
+
+//         for(int j= 0; j<=10 ;j++){
+//         std::cout << i <<'\n';}
+//         std::cout <<'\n';
+//     }
+   
+
+//     return 0;
+// }
+
+#include <iostream>
+int main(){
+    int rows;
+    int columns;
+    char symbol;
+
+    std::cout<<"how many rows ";
+    std::cin>>rows;
+
+    std::cout<<"how many columns ";
+    std::cin>>columns;
+
+    std::cout<<"enter a symbol to use  ";
+    std::cin>>symbol;
+
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=columns;j++){
+            std::cout<<symbol;
+        }
+        std::cout<<'\n';
+    }
+
+
+
+    return 0;
+}
