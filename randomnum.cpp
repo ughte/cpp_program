3c3dd52d1fc1804ffa5260e6271e36d0db40bf06
ughte <patiencenarh347@gmail.com>
2026-05-08 19:06:47 +0000

    using ctime function

---
+++ randomnum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ctime>//generation of pseudo (false) random numbers
+
+int main(){
+  srand(time(NULL));
+  int num1=(rand() % 6 )+ 1;//6 because i need random numbers between 1-6
+  int num1=(rand() % 6 )+ 1;
+  int num1=(rand() % 6 )+ 1;
+  std::cout<<num;
+  std::cout<<num;
+  std::cout<<num;
+    return 0;
+}
